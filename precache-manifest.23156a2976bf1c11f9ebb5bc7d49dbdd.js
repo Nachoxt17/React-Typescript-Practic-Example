937cd34525faf1058cfcfb9f1512f8b984b54f22
Nachoxt17 <nacho.man.kpo@hotmail.com>
2020-09-17 20:36:03 -0300

    Updates

---
+++ precache-manifest.23156a2976bf1c11f9ebb5bc7d49dbdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cc2074c32633170b02a267f62db7ba",
+    "url": "/React-Typescript-Practic-Example/index.html"
+  },
+  {
+    "revision": "8d74d78d99f6a6b23e6d",
+    "url": "/React-Typescript-Practic-Example/static/css/2.385b950e.chunk.css"
+  },
+  {
+    "revision": "b6c91c718a6be4751802",
+    "url": "/React-Typescript-Practic-Example/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d74d78d99f6a6b23e6d",
+    "url": "/React-Typescript-Practic-Example/static/js/2.07f159b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Typescript-Practic-Example/static/js/2.07f159b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c91c718a6be4751802",
+    "url": "/React-Typescript-Practic-Example/static/js/main.7e08cd50.chunk.js"
+  },
+  {
+    "revision": "9af782f8310724c88f5b",
+    "url": "/React-Typescript-Practic-Example/static/js/runtime-main.f0616543.js"
+  }
+]);
